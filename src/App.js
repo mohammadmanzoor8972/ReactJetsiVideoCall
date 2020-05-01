4f8175d46230f2dc2b1d397ad70eb8f210e65618
Mohammad Manzoor Alam <mohammad.manzoor8972@gmail.com>
2020-05-02 02:33:04 +0530

    refactor UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBIcon
+} from "mdbreact";
 
 import { Jutsu } from "react-jutsu";
 
@@ -16,28 +25,59 @@ function App() {
     <Jutsu
       roomName={room}
       userName={name}
+      width="100%"
+      height="100%"
       loadingComponent={<p>loading ...</p>}
     />
   ) : (
-    <form>
-      <input
-        id="room"
-        type="text"
-        placeholder="Room"
-        value={room}
-        onChange={e => setRoom(e.target.value)}
-      />
-      <input
-        id="name"
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={e => setName(e.target.value)}
-      />
-      <button onClick={handleClick} type="submit">
-        Start / Join
-      </button>
-    </form>
+    <>
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol md="6">
+            <MDBCard>
+              <MDBCardBody>
+                <form>
+                  <p className="h4 text-center py-4">Jitsi Conference</p>
+                  <label htmlFor="room" className="grey-text font-weight-light">
+                    Your Room
+                  </label>
+                  <input
+                    type="text"
+                    id="room"
+                    value={room}
+                    onChange={e => setRoom(e.target.value)}
+                    className="form-control"
+                    placeholder="Room"
+                  />
+                  <br />
+                  <label htmlFor="name" className="grey-text font-weight-light">
+                    Your Name
+                  </label>
+                  <input
+                    id="name"
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    className="form-control"
+                  />
+                  <div className="text-center py-4 mt-3">
+                    <MDBBtn
+                      onClick={handleClick}
+                      className="btn btn-outline-purple"
+                      type="submit"
+                    >
+                      Join
+                      <MDBIcon far icon="paper-plane" className="ml-2" />
+                    </MDBBtn>
+                  </div>
+                </form>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </>
   );
 }
 
